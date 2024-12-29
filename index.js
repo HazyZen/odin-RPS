898d719c85a904fe6a17cba51ddeded91955e295
HazyZen <vio.waqas@gmail.com>
2024-12-29 13:09:41 +0800

    Complete game

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-let humanChoice = prompt(
-  "Start the game by choosing rock, paper or scissors."
-).toLowerCase();
-
-let computerChoice = getComputerChoice();
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   let value = Math.floor(Math.random() * 3);
@@ -16,32 +13,35 @@ function getComputerChoice() {
   }
 }
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-      return `It's a tie! You both pick ${humanChoice}.
-      You: ${humanScore} | Computer: ${computerScore}`;
-    } else if (humanChoice === "rock" && computerChoice === "scissors") {
-      return `You win! Rock beats Scissors.
-      You: ${++humanScore} | Computer: ${computerScore}`;
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-      return `You win! Scissors beats Paper.
-      You: ${++humanScore} | Computer: ${computerScore}`;
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-      return `You win! Paper beats Rock.
-      You: ${++humanScore} | Computer: ${computerScore}`;
-    } else {
-      return `You lose! ${
-        computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
-      } beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}.
-      You: ${humanScore} | Computer: ${++computerScore}`;
-    }
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    return `It's a tie! You both pick ${humanChoice}.`;
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock")
+  ) {
+    humanScore++;
+    return `You win! ${
+      humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
+    } beats ${
+      computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+    }.`;
+  } else {
+    computerScore++;
+    return `You lose! ${
+      computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+    } beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}.`;
   }
-  console.log(playRound(humanChoice, computerChoice));
 }
-for (let i = 0; i < 5; i++) {
-  playGame();
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    let humanChoice = prompt("Choose Rock, Paper or Scissors.").toLowerCase();
+    let computerChoice = getComputerChoice();
+    console.log(playRound(humanChoice, computerChoice));
+    console.log(`You: ${humanScore} | Computer: ${computerScore}`);
+  }
 }
+
+playGame();
